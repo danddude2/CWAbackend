739bb4dda8d63f4931df5c89285dc18b8abf5216
Danny Thompson <dath7892@colorado.edu>
2017-02-16 20:06:46 -0700

    Updated working signup, and new database schema png

--- python/signup.py
+++ python/signup.py
@@ -9,32 +9,40 @@ form = cgi.FieldStorage()
 try: import simplejson as json
 except ImportError: import json
 
-data = { 'firstName':form.getvalue("firstName"), 'lastName':form.getvalue("lastName"), 'email':form.getvalue("email"),'phone':form.getvalue("phone"), 'password':form.getvalue("phone")}
+#print"Access-Control-Allow-Origin: http://cwajazz.com/vms"
+
+data = { 'firstName':form.getvalue("firstName"), 'lastName':form.getvalue("lastName"), 'email':form.getvalue("email"),'phone':form.getvalue("phone"), 'password':form.getvalue("password"), 'phone_type':form.getvalue("phoneProvider")}
 
 try:
     conn = MySQLdb.connect("localhost", "cwajazz9_vms", "radio#492*", "cwajazz9_vms")
     c = conn.cursor()
     
 except Exception as e:
-    print("Status: 500 Server Error\n")
+    print("Status: 503 Server Error\n")
     exit(1)
     
 if data["firstName"] and data["lastName"] and data["email"] and data["phone"] and data["password"]:
   
-    add_person = ("INSERT INTO VMS_persons(first_name,last_name,email,phone,password) VALUES(%s,%s,%s,%s,%s)")
-    check_person =  ("SELECT email FROM VMS_persons WHERE email=%s")
+    add_person = ("INSERT INTO person(name_first,name_last,email,phone_mobile) VALUES(%s,%s,%s,%s)")
+    check_person =  ("SELECT email FROM person WHERE email=%s")
     
-    add_person_values = (data['firstName'],data['lastName'],data['email'],data['phone'],data['password'])
+    add_person_values = (data['firstName'],data['lastName'],data['email'],data['phone'])
     check_person_values = (data['email'])
     
     try:
 		c.execute(check_person,check_person_values)
-		
+
 		if c.rowcount != 0:
 			print("Status: 202 Invalid email\n")			
 			
 		else:
 			c.execute(add_person,add_person_values)
+			person_pk = c.lastrowid
+			
+			add_person2 = ("INSERT INTO VMS_persons(person_pk,password,phone_type) VALUES(%s,%s,%s)")
+			add_person_values2 = (person_pk, data['password'],data['phone_type'])
+			
+			c.execute(add_person2,add_person_values2)						
 			conn.commit()			
 			print("Status: 200 OK\n")
         
