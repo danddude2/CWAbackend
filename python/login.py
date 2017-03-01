9e72803503325d503975c88a6a9135f67c33650d
Justin Tang <justin.tang@colorado.edu>
2017-02-28 17:07:57 -0700

    changed login to send back eventIds and personId

--- python/login.py
+++ python/login.py
@@ -8,6 +8,8 @@ import cgitb; cgitb.enable()
 form = cgi.FieldStorage()
 email = form.getvalue("email")
 attemptedPassword = form.getvalue("password")
+#email = "volunteer"
+#attemptedPassword = "root"
 data = {}
 
 try:
@@ -20,7 +22,8 @@ except Exception as e:
 getSaltSQL = "SELECT salt FROM VMS_persons WHERE person_pk =%s"
 getPasswordSQL ="SELECT password FROM VMS_persons WHERE person_pk=%s"
 getAdminSQL ="SELECT admin_status FROM VMS_persons WHERE person_pk=%s"
-getPkSQL = "SELECT person_pk from person WHERE email = %s"
+getPkSQL = "SELECT person_pk FROM person WHERE email = %s"
+getEvents = "SELECT event_id FROM VMS_voluteer_availability WHERE person_pk = %s"
 
 try:
 	cursor.execute(getPkSQL,[email])
@@ -56,14 +59,30 @@ except Exception as e:
 	exit(1)
 
 try:
+	cursor = connection.cursor()
+	cursor.execute(getEvents,key)
+	events = []
+	event_cursor = cursor.fetchall()
+	for ids in event_cursor:
+		[e] = ids
+		events.append(e)
+		events = list(set(events))
+	cursor.close()
+except Exception as e:
+	connection.rollback()
+	print("Status: 400 Invalid SQL Statment\n")
+	print e
+	exit(1)
+
+try:
 	if p == hashlib.sha512(s + attemptedPassword).hexdigest():
 		cursor = connection.cursor()
 		cursor.execute(getAdminSQL,key)
 		(status,) = cursor.fetchone()
 		if(status == 0):
-			data.update({'isAdmin':False})
+			data.update({'isAdmin':False,'personID':key, 'eventIds':events})
 		else:
-			data.update({'isAdmin':True})
+			data.update({'isAdmin':True,'personID':key, 'eventIds':events})
 		print"Content-type: application/json"
 		print"Status: 200 Login OK"
 		print ""
